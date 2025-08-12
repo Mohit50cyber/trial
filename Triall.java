767cac49af0696f1f3099e580fe5db6023fb1de1
Mohit50cyber <68845963+Mohit50cyber@users.noreply.github.com>
2025-08-12 14:11:28 +0530

    4 v

--- Triall.java
+++ Triall.java
@@ -1,9 +1,16 @@
+import java.sql.Time;
+import java.util.Date;
+
 public class Triall {
 
     public static void main(String[] args) {
         System.out.println("My name is Mohit Singh");
         System.out.println("-----------------------");
         System.out.println("Today is 12 August 2025");
+        System.out.println("--------------------------");
+        System.out.println("--------------------------");
+        System.out.println(new Date());
+        
     }
     
 }
