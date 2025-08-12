7c53ee33c28c89f48d621b22ca36b250a3eda8d0
Mohit50cyber <68845963+Mohit50cyber@users.noreply.github.com>
2025-08-12 13:51:10 +0530

    1 v

---
+++ trial.java
@@ -0,0 +1,8 @@
+public class trial {
+
+    public static void main(String[] args) {
+        System.out.println("We the people of India having solemnly resolved to constitute India into"+
+        "a sovereign , socialistic , secular and democratic republic");
+    }
+    
+}
